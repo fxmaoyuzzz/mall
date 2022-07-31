acf6040bdf7e0c24737587759bb64aca8a1d93aa
lizenghai <fxmaoyuzzz@126.com>
2022-07-31 13:12:13 +0800

    项目构建

---
+++ mall-member/src/test/java/com/moyu/mall/member/MallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moyu.mall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
