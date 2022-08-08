d771312232e0bbebf531618e03ae12025a57c9f4
lizenghai <fxmaoyuzzz@126.com>
2022-08-08 23:56:34 +0800

    配置中心测试

--- mall-product/src/main/java/com/moyu/mall/product/service/CategoryService.java
+++ mall-product/src/main/java/com/moyu/mall/product/service/CategoryService.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.moyu.common.utils.PageUtils;
 import com.moyu.mall.product.entity.CategoryEntity;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,5 +17,11 @@ import java.util.Map;
 public interface CategoryService extends IService<CategoryEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    /**
+     * 查出所有分类及子分类，并组装树形结构
+     * @return
+     */
+    List<CategoryEntity> listWithTree();
 }
 
--- mall-product/src/main/java/com/moyu/mall/product/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/com/moyu/mall/product/service/impl/CategoryServiceImpl.java
@@ -1,20 +1,24 @@
 package com.moyu.mall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.moyu.common.utils.PageUtils;
 import com.moyu.common.utils.Query;
-
 import com.moyu.mall.product.dao.CategoryDao;
 import com.moyu.mall.product.entity.CategoryEntity;
 import com.moyu.mall.product.service.CategoryService;
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Service("categoryService")
 public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
+
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -24,6 +28,42 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+
+        //组装树形结构
+        if (CollectionUtils.isNotEmpty(entities)){
+            List<CategoryEntity> level1Menus = entities.stream().filter(item ->
+                item.getParentCid() == 0
+            ).map(menu -> {
+                menu.setChildren(getChildren(menu, entities));
+
+                return menu;
+            }).sorted((menu1, menu2) -> {
+                return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
+            }).collect(Collectors.toList());
+
+            return level1Menus;
+        }
+        return entities;
+    }
+
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all){
+        List<CategoryEntity> children = all.stream().filter(item -> {
+            return item.getParentCid() == root.getCatId();
+        }).map(category -> {
+            category.setChildren(getChildren(category, all));
+
+            return category;
+        }).sorted((category1, category2) -> {
+            return (category1.getSort() == null ? 0 : category1.getSort()) - (category2.getSort() == null ? 0 :category2.getSort());
+        }).collect(Collectors.toList());
+
+        return children;
     }
 
 }
