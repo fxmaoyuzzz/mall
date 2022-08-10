85cfb88be2ba807a977cc57b1314552415809581
lizenghai <fxmaoyuzzz@126.com>
2022-08-11 00:30:29 +0800

    OSS上传服务

--- mall-product/src/main/java/com/moyu/mall/product/service/impl/BrandServiceImpl.java
+++ mall-product/src/main/java/com/moyu/mall/product/service/impl/BrandServiceImpl.java
@@ -1,5 +1,6 @@
 package com.moyu.mall.product.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -8,11 +9,13 @@ import com.moyu.common.utils.Query;
 import com.moyu.mall.product.dao.BrandDao;
 import com.moyu.mall.product.entity.BrandEntity;
 import com.moyu.mall.product.service.BrandService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 
+@Slf4j
 @Service
 public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> implements BrandService {
 
@@ -22,6 +25,7 @@ public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> impleme
                 new Query<BrandEntity>().getPage(params),
                 new QueryWrapper<BrandEntity>()
         );
+        log.info("查询品牌数据:{}", JSONObject.toJSONString(page));
 
         return new PageUtils(page);
     }
