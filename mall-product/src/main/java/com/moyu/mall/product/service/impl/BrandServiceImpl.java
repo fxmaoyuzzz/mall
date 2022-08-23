8305f46161ca61fc5106e9e4fc3481d21fa31f15
lizenghai <fxmaoyuzzz@126.com>
2022-08-23 21:23:03 +0800

    品牌管理

--- mall-product/src/main/java/com/moyu/mall/product/service/impl/BrandServiceImpl.java
+++ mall-product/src/main/java/com/moyu/mall/product/service/impl/BrandServiceImpl.java
@@ -1,7 +1,7 @@
 package com.moyu.mall.product.service.impl;
 
 import com.alibaba.fastjson.JSONObject;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.moyu.common.utils.PageUtils;
@@ -10,6 +10,7 @@ import com.moyu.mall.product.dao.BrandDao;
 import com.moyu.mall.product.entity.BrandEntity;
 import com.moyu.mall.product.service.BrandService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -21,10 +22,20 @@ public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> impleme
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        String key = (String) params.get("key");
+        LambdaQueryWrapper<BrandEntity> wrapper = new LambdaQueryWrapper<>();
+
+        if (StringUtils.isNotBlank(key)) {
+            wrapper.eq(BrandEntity::getBrandId, key)
+                    .or()
+                    .like(BrandEntity::getName, key);
+        }
+
         IPage<BrandEntity> page = this.page(
                 new Query<BrandEntity>().getPage(params),
-                new QueryWrapper<BrandEntity>()
+                wrapper
         );
+
         log.info("查询品牌数据:{}", JSONObject.toJSONString(page));
 
         return new PageUtils(page);
